2a018f051e357a5defc5dfa281b9c10aaba7ee00
Grigory Golikov <golikov@nextbi.ru>
2023-07-06 15:22:19 +0300

    Add contextmenu actions

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,33 +1,137 @@
+import { feature } from '@turf/turf';
 import Mgt from '../../src/index.js';
+import rings from './data/rings.json';
+const mgt = new Mgt();
 
-const map = L.map('map').setView([55.755864, 37.617698], 12);
+const ringNames = ["Red Square", "Kremlin", "Boulevard Ring", "Garden Ring", "Third Transport Ring", "MKAD"];
+
+const state = {
+	rings: ringNames.reduce((prev, cur) => ({
+		...prev,
+		[cur]: {
+			feature: rings.features.find(f => f.properties.name === cur),
+			layer: null,
+		}
+	}), {})
+};
+
+function getStrokeColor(ringName) {
+switch (ringName) {
+	case "Red Square":
+		return '#FF0000';
+	case "Kremlin":
+		return '#FFC300';
+	case "Boulevard Ring":
+		return '#FFFF00';
+	case "Garden Ring":
+		return '#13FF04';
+	case "Third Transport Ring":
+		return '#00B97B';
+	case "MKAD":
+		return '#0346F8';
+	default:
+		break;
+}}
+
+const baseStyle = {
+	weight: 3,
+	opacity: 1,
+	fillOpacity: 0
+};
+
+const mapOptions = {
+	contextmenu: true,
+	contextmenuWidth: 200,
+	contextmenuItems: [
+		{
+			text: 'Очистить',
+			callback: clearLayers,
+		},
+		{
+			text: 'Буфер от Красной площади',
+			callback: drawBufferFromRing.bind(null, 'Red Square'),
+		},
+		{
+			text: 'Буфер от Кремля',
+			callback: drawBufferFromRing.bind(null, 'Kremlin'),
+		},
+		{
+			text: 'Буфер от Бульварного кольца',
+			callback: drawBufferFromRing.bind(null, 'Boulevard Ring'),
+		},
+		{
+			text: 'Буфер от Садового кольца',
+			callback: drawBufferFromRing.bind(null, 'Garden Ring'),
+		},
+		{
+			text: 'Буфер от ТТК',
+			callback: drawBufferFromRing.bind(null, 'Third Transport Ring'),
+		},
+		{
+			text: 'Буфер от МКАД',
+			callback: drawBufferFromRing.bind(null, 'MKAD'),
+		},
+	],
+};
+
+const map = L.map('map', mapOptions).setView([55.755864, 37.617698], 12);
 const urls = [
 	'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+	'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
 	'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
 	'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
 	'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
 	'https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png',
 	'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',
 ];
-const baseLayer = L.tileLayer(urls[0], {
+
+const baseLayer = L.tileLayer(urls[1], {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+function clearLayers() {
+	Object.keys(state.rings).forEach((ringName) => {
+		const obj = state.rings[ringName];
+
+		if (obj.layer) {
+			map.removeLayer(obj.layer);
+			obj.layer = null;
+		} 
+	})
+
+	console.log(map._layers);
+}
+
+function drawBufferFromRing(ringName, e) {
+	const json = mgt.getBufferAtPoint(e.latlng, state.rings[ringName].feature);
+	
+	if (state.rings[ringName].layer) {
+		map.removeLayer(state.rings[ringName].layer);
+	}
+
+	const layer = L.geoJson(json, {
+		style: function () {
+			return {
+				...baseStyle, color: getStrokeColor(ringName),  dashArray: '10', }
+		}
+	});
+
+	state.rings[ringName].layer = layer;
+	
+	layer.addTo(map);
+}
+
+// L.geoJson(atd).addTo(map);
+L.geoJson(rings, {
+	style: function (feature) {
+		return { ...baseStyle, color: getStrokeColor(feature.properties.name) }
+	}
+}).addTo(map);
+
 map.addLayer(baseLayer);
 
 window.map = map;
-
-var latlngs = [
-	[37, -109.05],
-	[41, -109.03],
-	[41, -102.05],
-	[37, -102.04],
-];
-
-var polygon = L.polygon(latlngs, { color: 'red' }).addTo(map);
-const mgt = new Mgt();
-
-mgt.greet();
+window.mgt = mgt;
 
 console.log(mgt);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
+import * as turf from '@turf/turf';
 export default class Mgt {
-    greet() {
-        console.log('greet');
+    static bufferSteps = 200;
+
+    static distanceToPolygon({ point, polygon }) {
+        if (polygon.type === "Feature") { polygon = polygon.geometry }
+        let distance;
+        if (polygon.type === "MultiPolygon") {
+            distance = polygon.coordinates
+                .map(coords => distanceToPolygon({ point, polygon: turf.polygon(coords).geometry }))
+                .reduce((smallest, current) => (current < smallest ? current : smallest));
+        } else {
+            if (polygon.coordinates.length > 1) {
+                const [exteriorDistance, ...interiorDistances] = polygon.coordinates.map(coords =>
+                    distanceToPolygon({ point, polygon: turf.polygon([coords]).geometry })
+                );
+                if (exteriorDistance < 0) {
+                    const smallestInteriorDistance = interiorDistances.reduce(
+                        (smallest, current) => (current < smallest ? current : smallest)
+                    );
+                    if (smallestInteriorDistance < 0) {
+                        distance = smallestInteriorDistance * -1;
+                    } else {
+                        distance = smallestInteriorDistance < exteriorDistance * -1
+                            ? smallestInteriorDistance * -1
+                            : exteriorDistance;
+                    }
+                } else {
+                    distance = exteriorDistance;
+                }
+            } else {
+                // The actual distance operation - on a normal, hole-less polygon (converted to meters)
+                distance = turf.pointToLineDistance(point, turf.polygonToLineString(polygon));
+                if (turf.booleanPointInPolygon(point, polygon)) {
+                    distance = distance * -1;
+                }
+            }
+        }
+        return distance
+    }
+
+    getBufferAtPoint(latLng, geoJson) {
+        const point = turf.point([latLng.lng, latLng.lat]);
+        const distance = Mgt.distanceToPolygon({ point, polygon: geoJson });
+
+        return turf.buffer(geoJson, distance, {steps: Mgt.bufferSteps});
     }
 }
